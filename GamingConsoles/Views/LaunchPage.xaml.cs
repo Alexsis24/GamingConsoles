f80f2bce59feb3d485fdffab1863d9a6f536137f
Estlib <75094683+Estlib@users.noreply.github.com>
2023-04-27 13:37:37 +0300

    added base styles for each font

--- GamingConsoles/Views/LaunchPage.xaml.cs
+++ GamingConsoles/Views/LaunchPage.xaml.cs
@@ -2,22 +2,10 @@ namespace GamingConsoles.Views;
 
 public partial class LaunchPage : ContentPage
 {
-	int count = 0;
 
 	public LaunchPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
